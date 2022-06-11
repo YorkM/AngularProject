3d8fb54f21ba114320d1b12c8e87d2ddf1f4d0bf
209 <209@academicos.cesde.edu.co>
2022-06-11 16:45:34 -0500

    Hoteles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component';
+import { ServicioshotelComponent } from './servicioshotel/servicioshotel.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormularioreservaComponent } from './formularioreserva/formularioreserva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    HabitacionesComponent,
+    ServicioshotelComponent,
+    FooterComponent,
+    FormularioreservaComponent
   ],
   imports: [
     BrowserModule,
